9b9b8586d6b4f7b7cda36b413b4b2feafa3b7214
larcondo <larcondo90@gmail.com>
2023-09-18 17:22:52 -0300

    exercise 8.16
    add user management

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { startStandaloneServer } = require('@apollo/server/standalone')
 const mongoose = require('mongoose')
 const Book = require('./models/book')
 const Author = require('./models/author')
+const User = require('./models/user')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const { GraphQLError } = require('graphql')
 mongoose.set('strictQuery', false)
@@ -27,11 +29,22 @@ const typeDefs = `
     bookCount: Int
   }
 
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+    me: User
   }
 
   type Mutation {
@@ -45,6 +58,14 @@ const typeDefs = `
       name: String!
       setBornTo: Int!
     ): Author
+    createUser (
+      username: String!
+      favoriteGenre: String!
+    ): User
+    login (
+      username: String!
+      password: String
+    ): Token
   }
 `
 
@@ -75,10 +96,16 @@ const resolvers = {
           bookCount: books.filter(b => b.author.toString() === a._id.toString()).length
         }
       })
-    }
+    },
+    me: (root, args, context) => context.currentUser
   },
   Mutation: {
-    addBook: async (root, args) => {
+    addBook: async (root, args, {currentUser}) => {
+      if (!currentUser)
+        throw new GraphQLError('not authenticated', {
+          extensions: { code: 'BAD_USER_INPUT', }
+        })
+        
       const author = await Author.findOne({ name: args.author })
 
       const newAuthor = new Author({ name: args.author, born: null })
@@ -112,7 +139,13 @@ const resolvers = {
 
       return book
     },
-    editAuthor: async (root, args) => {
+    editAuthor: async (root, args, {currentUser}) => {
+      if (!currentUser) 
+        throw new GraphQLError('not authenticated', {
+          extensions: { code: 'BAD_USER_INPUT', }
+        })
+      
+
       const author = await Author.findOne({ name: args.name })
       if (!author) return null
 
@@ -130,6 +163,41 @@ const resolvers = {
       }
 
       return author
+    },
+    createUser: async (root, args) => {
+      const user = new User({ 
+        username: args.username,
+        favoriteGenre: args.favoriteGenre
+      })
+
+      return user.save()
+        .catch( error => {
+          throw new GraphQLError('Creating user failed', {
+            extensions: {
+              code: 'BAD_USER_INPUT',
+              invalidArgs: args.username,
+              error
+            }
+          })
+        })
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+
+      if (!user || args.password !== 'secret') {
+        throw new GraphQLError('wrong credentials', {
+          extensions: {
+            code: 'BAD_USER_INPUT'
+          }
+        })
+      }
+
+      const userForToken = {
+        username: args.username,
+        id: user._id
+      }
+
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET)}
     }
   }
 }
@@ -141,6 +209,14 @@ const server = new ApolloServer({
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
+  context: async ({ req, res }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.startsWith('Bearer ')) {
+      const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
+      const currentUser = await User.findById(decodedToken.id)
+      return { currentUser }
+    }
+  }
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const uniqueValidator = require('mongoose-unique-validator')
+
+const schema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: 3
+  },
+  favoriteGenre: {
+    type: String,
+    minLength: 2
+  }
+})
+
+schema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('User', schema)
